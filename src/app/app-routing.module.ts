6c1c7f3bbe9e51a4ab5896ef095a8c7d506156a9
lithdoh <lithifuse@gmail.com>
2022-08-17 08:28:56 -0700

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FrameComponent } from './parts/frame/frame.component';
+import { RearshockComponent } from './parts/rearshock/rearshock.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'parts/frame', component: FrameComponent },
+  { path: 'parts/rearshock', component: RearshockComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
