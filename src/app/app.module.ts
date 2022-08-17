d62b77dddb627bb538b1f8aa03f2e450d9d1c546
lithdoh <lithifuse@gmail.com>
2022-08-17 08:28:41 -0700

    Add components and routermodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FrameComponent } from './parts/frame/frame.component';
+import { RearshockComponent } from './parts/rearshock/rearshock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FrameComponent,
+    RearshockComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
