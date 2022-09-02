954c7a5c2325c9c1aa1bdcaa92e53e8374c184ba
Mitch K <lithifuse@gmail.com>
2022-09-02 11:14:58 -0700

    Rename stemComponent and add matbars and matstems routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { BarsComponent } from './parts/bars/bars.component';
 import { HomeComponent } from './home/home.component';
 import { FrameComponent } from './parts/frame/frame.component';
 import { RearshockComponent } from './parts/rearshock/rearshock.component';
-import { StemComponent } from './parts/stem/stem.component';
+import { StemsComponent } from './parts/stems/stems.component';
 import { MATbarsComponent } from './parts/matbars/matbars.component';
 import { MatstemsComponent } from './parts/matstems/matstems.component';
 import { MathomeComponent } from './mathome/mathome.component';
@@ -13,12 +13,13 @@ const routes: Routes = [
   { path: 'parts/frame', component: FrameComponent },
   { path: 'parts/rearshock', component: RearshockComponent },
   { path: 'parts/bars', component: BarsComponent },
-  { path: 'parts/stem', component: StemComponent },
+  { path: 'parts/stems', component: StemsComponent },
   { path: 'parts/rearbrake', component: MATbarsComponent },
   { path: 'home', component: HomeComponent },
   { path: 'parts/frontbrake', component: MatstemsComponent },
   { path: 'parts/chain', component: MathomeComponent },
-
+  { path: 'parts/matbars', component: MATbarsComponent },
+  { path: 'parts/matstems', component: MatstemsComponent },
 ];
 
 @NgModule({
