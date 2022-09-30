ab3cf5d5ca47e853df5c3c1428e181b858a25a52
lithdoh <lithifuse@gmail.com>
2022-09-30 07:38:11 -0700

    Remove myService.service and imports and reference

--- src/app/mathome/mathome.component.ts
+++ src/app/mathome/mathome.component.ts
@@ -2,7 +2,6 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { MyServiceService } from '../my-service.service';
 
 export interface PeriodicElement {
   component: string;
@@ -46,7 +45,6 @@ export class MathomeComponent implements AfterViewInit {
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
-    public myService: MyServiceService,
 
     ) { }
 
