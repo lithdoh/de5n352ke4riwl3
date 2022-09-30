defa6f3687e8db91a54544e3de7ffe77184c3233
lithdoh <lithifuse@gmail.com>
2022-09-30 07:39:18 -0700

    Stems model init

---
+++ src/app/models/stems.model.ts
@@ -0,0 +1,15 @@
+export interface Stems {
+  image: string;
+  name: string;
+  brand: string;
+  model: string;
+  barClampDiameter: number;
+  length: number;
+  rise: number;
+  steererTubeDiameter: string;
+  color: string;
+  material: string;
+  price: number;
+  weight: number;
+  where: string;
+}
