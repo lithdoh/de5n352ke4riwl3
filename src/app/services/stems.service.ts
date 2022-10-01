a7783ed448c4888d5978bb2ebbea6087c2c713e8
lithdoh <lithifuse@gmail.com>
2022-09-30 18:37:07 -0700

    Comment out test code

--- src/app/services/stems.service.ts
+++ src/app/services/stems.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient }   from '@angular/common/http';
 import { map, Observable }   from 'rxjs';
 import { Stems } from '../models/stems.model';
+import { SortDirection } from '@angular/material/sort';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StemsService {
-  private serviceUrl = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query={ queryStem { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }';
+  // private serviceUrl = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query={ queryStem { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }';
 
   constructor(private http: HttpClient) { }
 
-  getStems(sort: string, order: SortDirection, page: number): Observable<Stems[]> {
-    const requestURL = `${this.serviceUrl}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
-      page + 1
-    }`
+  getStems(): Observable<Stems[]> {
+    const requestURL = `https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query={ queryStem { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }`;
     return this.http.get<Stems[]>(requestURL).pipe(map((response: any) => response.data.queryStem));
   }
+  // getStems(sort: string, order: SortDirection, page: number): Observable<Stems[]> {
+  //   const requestURL = `${this.serviceUrl}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
+  //     page + 1
+  //   }`
+  //   return this.http.get<Stems[]>(requestURL).pipe(map((response: any) => response.data.queryStem));
+  // }
 }
