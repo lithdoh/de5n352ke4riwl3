8b249dc03e766040ed959cc7ff94aa3c4dd66799
Mitch K <lithifuse@gmail.com>
2022-09-30 18:46:41 -0700

    Comment out testing code

--- src/app/services/stems.service.ts
+++ src/app/services/stems.service.ts
@@ -7,14 +7,18 @@ import { Stems } from '../models/stems.model';
   providedIn: 'root'
 })
 export class StemsService {
-  private serviceUrl = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query={ queryStem { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }';
+  // private serviceUrl = 'query { queryStem(filter: {brand: {alloftext: "Renthal"}}, first: 2, offset: 0, order: {asc: length}) { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } } ';
 
   constructor(private http: HttpClient) { }
 
-  getStems(sort: string, order: SortDirection, page: number): Observable<Stems[]> {
-    const requestURL = `${this.serviceUrl}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
-      page + 1
-    }`
+  getStems(): Observable<Stems[]> {
+    const requestURL = `https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query={ queryStem(filter: {brand: {alloftext: "Renthal"}}, first: 2, offset: 0, order: {asc: length}) { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }`;
     return this.http.get<Stems[]>(requestURL).pipe(map((response: any) => response.data.queryStem));
   }
+  // getStems(sort: string, order: SortDirection, page: number): Observable<Stems[]> {
+  //   const requestURL = `${this.serviceUrl}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
+  //     page + 1
+  //   }`
+  //   return this.http.get<Stems[]>(requestURL).pipe(map((response: any) => response.data.queryStem));
+  // }
 }
