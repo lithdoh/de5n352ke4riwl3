95ee7478f1edeba5f76a54087625345198ec0487
lithdoh <lithifuse@gmail.com>
2022-10-28 13:38:34 -0700

    Remove sorting from mathome

--- src/app/mathome/mathome.component.ts
+++ src/app/mathome/mathome.component.ts
@@ -1,6 +1,4 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
+import { AfterViewInit, Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
@@ -21,7 +19,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     price: '',
     weight: '',
     priceWeight: '',
-    where: ''
+    where: '',
   },
   {
     component: 'Stems',
@@ -30,44 +28,31 @@ const ELEMENT_DATA: PeriodicElement[] = [
     price: '',
     weight: '',
     priceWeight: '',
-    where: ''
+    where: '',
   },
 ];
 
 @Component({
   selector: 'app-mathome',
   templateUrl: './mathome.component.html',
-  styleUrls: ['./mathome.component.css']
+  styleUrls: ['./mathome.component.css'],
 })
 export class MathomeComponent implements AfterViewInit {
-  displayedColumns: string[] = ['component', 'selection', 'price', 'weight', 'priceWeight', 'where', 'add', 'remove'];
+  displayedColumns: string[] = [
+    'component',
+    'selection',
+    'price',
+    'weight',
+    'priceWeight',
+    'where',
+    'add',
+    'remove',
+  ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-  constructor(
-    private _liveAnnouncer: LiveAnnouncer,
-
-    ) { }
-
-  @ViewChild(MatSort) sort!: MatSort;
-
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-
-  }
-
-  /** Announce the change in sort state for assistive technology. */
-  announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
-
-  // partinfo!: PartInfo[];
+  
+  mainView = true;
+  constructor() {}
+  
+  ngAfterViewInit() {}
+  
 }
