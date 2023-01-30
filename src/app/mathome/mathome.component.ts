7612e96ea233a9e22148a688530487b87c5f80b8
lithdoh <lithifuse@gmail.com>
2023-01-29 16:12:32 -0800

    Add logic to get stem object from localStorage

--- src/app/mathome/mathome.component.ts
+++ src/app/mathome/mathome.component.ts
@@ -1,20 +1,25 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Stems } from '../models/stems.model';
 
 export interface PeriodicElement {
   component: string;
   link: string;
+  image: string;
   selection: string;
-  price: string;
-  weight: string;
-  priceWeight: string;
+  price: string | number; // I want these properties to not display anything until they have a value set to them, i.e. I want '' to display not 0
+  weight: string | number;
+  priceWeight: string | number;
   where: string;
 }
+// What if you make some of these optional to solve the problem of not having the default...?
 
+// Should I use a Set instead of an Array?
 const ELEMENT_DATA: PeriodicElement[] = [
   {
     component: 'Bars',
     link: '/parts/matbars',
+    image: '',
     selection: 'Choose Bars',
     price: '',
     weight: '',
@@ -23,7 +28,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   },
   {
     component: 'Stems',
-    link: '/parts/matstems',
+    link: '/parts/matstems2',
+    image: '',
     selection: 'Choose a Stem',
     price: '',
     weight: '',
@@ -37,7 +43,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './mathome.component.html',
   styleUrls: ['./mathome.component.css'],
 })
-export class MathomeComponent implements AfterViewInit {
+export class MathomeComponent implements OnInit {
   displayedColumns: string[] = [
     'component',
     'selection',
@@ -49,10 +55,27 @@ export class MathomeComponent implements AfterViewInit {
     'remove',
   ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
-  
+
+  // When this function is called, the table needs to refresh to show that the selection is now nothing. Can I use the Window: storage event?
+  clearStem() {
+    localStorage.removeItem('stem');
+    window.location.reload(); // Works, but should I really be reloading the whole page?
+  }
+
   mainView = true;
   constructor() {}
   
-  ngAfterViewInit() {}
+  ngOnInit() {
+    const STEM: Stems = JSON.parse(localStorage.getItem('stem') || '{}');
+    ELEMENT_DATA[1].image = STEM.image ? STEM.image : '';
+    ELEMENT_DATA[1].selection = STEM.name ? STEM.name : 'Choose a Stem';
+    ELEMENT_DATA[1].price = STEM.price ? STEM.price : '';
+    ELEMENT_DATA[1].weight = STEM.weight ? STEM.weight : '';
+    ELEMENT_DATA[1].priceWeight = (STEM.price / STEM.weight) ? (STEM.price / STEM.weight) : '';
+    ELEMENT_DATA[1].where = STEM.where ? STEM.where : '';
+  }
+
+  // If there is a not stem set in local storage, display "Choose a Stem", 
+  // otherwise, display the Stem's image, name, price, weight, and where properties.
   
 }
