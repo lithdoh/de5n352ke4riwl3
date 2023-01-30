1f22ff267c2fc06ee7ce5a89154b483c56b6c251
lithdoh <lithifuse@gmail.com>
2023-01-30 08:54:03 -0800

    Add comments, make addItem() work, move baseURL

--- src/app/parts/matstems2/matstems2.component.ts
+++ src/app/parts/matstems2/matstems2.component.ts
@@ -1,11 +1,11 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort, Sort, SortDirection } from '@angular/material/sort';
 import { Router } from '@angular/router';
-import { startWith, switchMap, catchError, map, Observable, of, merge, tap } from 'rxjs';
-import { Stems } from 'src/app/models/stems.model';
+import { startWith, switchMap, catchError, map, Observable, of, merge, tap, fromEvent } from 'rxjs';
+import { Stems } from '../../models/stems.model';
 
 @Component({
   selector: 'app-matstems2',
@@ -14,7 +14,6 @@ import { Stems } from 'src/app/models/stems.model';
 })
 export class Matstems2Component implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
-    // 'id',
     'image',
     'name',
     'brand',
@@ -35,11 +34,12 @@ export class Matstems2Component implements OnInit, AfterViewInit {
 
   length: number = 0;
   pageSizeOptions: number[] = [5, 10, 25, 100];
-  showFirstLastButtons = true;
-  isLoadingResults = true;
+  showFirstLastButtons: boolean = true;
+  isLoadingResults: boolean = true;
 
   @ViewChild(MatSort, {static: true}) sort!: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // @ViewChild('toTarget') toTarget!: ElementRef;
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
@@ -50,10 +50,11 @@ export class Matstems2Component implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
     // Can I put these initial values in the template on the MatSort directive?
+    // I have to set these or it won't work. Why doesn't the "StartWith" set the default?
     this.sort.direction = 'asc';
     this.sort.active = 'name';
 
-    // Set length of paginator
+    // Set length of paginator, what should the type be for x?
     this.exampleDatabase.getCount().subscribe(x => this.length = x.count);
   }
   
@@ -62,6 +63,8 @@ export class Matstems2Component implements OnInit, AfterViewInit {
 
     // If the user changes the sort order, reset back to the first page.
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    // fromEvent(this.toTarget.nativeElement, 'keyup').subscribe(console.log);
 
     merge(this.sort.sortChange, this.paginator.page)
       .pipe(
@@ -85,7 +88,7 @@ export class Matstems2Component implements OnInit, AfterViewInit {
       )
       .subscribe(data => (this.data = data));
 
-    // Attempt to do Sorting and Pagination the way that "Sorting Only" does it (see below)
+    // Attempt to do Sorting and Pagination the way that "Sorting Only" does it (see "Sorting Only" below)
     // merge(this.sort.sortChange, this.paginator.page)
     //   .pipe(
     //     startWith({direction: 'asc', active: 'name', pageIndex: 0, pageSize: 5} as Sort | PageEvent),
@@ -131,28 +134,28 @@ export class Matstems2Component implements OnInit, AfterViewInit {
   }
 
   /** Adding products to the list: */
-  onAddItem(x: object) {
+  addItem(stemObject: object) {
     // “add” button invokes function (event binding in matstems template)
     // function accesses the list of stems (stem component)
-    console.log(x)
-
-    /* From talk with Steve
-      this.id.emit(id);
-    */
+    
+    const stemString: string = JSON.stringify(stemObject);
+    /*   Should I put this in a "try...catch" block because it says 
+    "Throws a "QuotaExceededError" DOMException exception if the new value couldn't be set."? */
+    localStorage.setItem('stem', stemString);
 
     // and adds a product to the build list (home component)
 
     // route to the page
-    // this.router.navigate(['/parts/chain']);
+    this.router.navigate(['/parts/chain']);
   }
 
-  // Alternatively:
-  // When you click the add button, the id of the item is stored, and you are redirected to the "list" page.
-  // On the list page, a request is sent to the database for the item with the stored id.
-  // That item is displayed.
-  // Another way
-  // When you click the add button, the item whose add button you clicked is stored as an object, and you are redirected to the "list" page
-  // That item is displayed.
+  /* Alternatively:
+  When you click the add button, the id of the item is stored, and you are redirected to the "list" page.
+  On the list page, a request is sent to the database for the item with the stored id.
+  That item is displayed.
+  Another way
+  When you click the add button, the item whose add button you clicked is stored as an object, and you are redirected to the "list" page
+  That item is displayed. */
 
   /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
@@ -171,17 +174,21 @@ export class Matstems2Component implements OnInit, AfterViewInit {
 // Sorting and Pagination
 export class ExampleHttpDatabase {
   constructor(private http: HttpClient) {}
+  baseURL = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query=';
 
   getStems(order: SortDirection, column: string, pageSize: number, pageIndex: number): Observable<Stems[]> {
-    const baseURL = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query=';
-    const requestURL = baseURL + `{ queryStem(order: {${order}: ${column}}, first: ${pageSize}, offset: ${pageIndex*pageSize}) 
+    // RequestURL with filtering
+    // const requestURL = this.baseURL + `{ queryStem(order: {${order}: ${column}}, first: ${pageSize}, offset: ${pageIndex*pageSize}, filter: {name: {regexp: "/an/i"}, or: {material: {regexp: "/an/i"}, or: {brand: {regexp: "/an/i"}, or: {color: {regexp: "/an/i"}, or: {model: {regexp: "/an/i"}}}}}}) 
+    // { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }`;
+    
+    const requestURL = this.baseURL + `{ queryStem(order: {${order}: ${column}}, first: ${pageSize}, offset: ${pageIndex*pageSize}) 
     { barClampDiameter brand color image length material model name price rise steererTubeDiameter weight where } }`;
     return this.http.get<Stems[]>(requestURL).pipe(map((response: any) => response.data.queryStem));
   }
 
   getCount() {
-    const baseURL = 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query=';
-    const requestURL = baseURL + `{ aggregateStem { count }}`;
+    const requestURL = this.baseURL + `{ aggregateStem { count }}`;
+    console.log(this.http.get<string>(requestURL).pipe(map((response: any) => response.data.aggregateStem)))
     return this.http.get<string>(requestURL).pipe(map((response: any) => response.data.aggregateStem));
   }
 }
