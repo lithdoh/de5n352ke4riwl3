97acc050e19286fbbcea311d845358a2db8929d7
lithdoh <lithifuse@gmail.com>
2023-02-03 11:41:42 -0800

    Rearrange imports, add matSideNav and MatCheckBox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTableModule } from '@angular/material/table';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MathomeComponent } from './mathome/mathome.component';
@@ -17,14 +23,16 @@ import { BarsComponent } from './parts/bars/bars.component';
 import { FrameComponent } from './parts/frame/frame.component';
 import { MATbarsComponent } from './parts/matbars/matbars.component';
 import { MatstemsComponent } from './parts/matstems/matstems.component';
+import { Matstems2Component } from './parts/matstems2/matstems2.component';
+import { Matstems3Component } from './parts/matstems3/matstems3.component';
 import { RearshockComponent } from './parts/rearshock/rearshock.component';
 import { StemsComponent } from './parts/stems/stems.component';
 import { DegreePipe } from './pipes/degree.pipe';
-import { MillimeterPipe } from './pipes/millimeter.pipe';
-import { Matstems2Component } from './parts/matstems2/matstems2.component';
-import { GramsPipe } from './pipes/grams.pipe';
 import { DollarsPerGramPipe } from './pipes/dollars-per-gram.pipe';
-import {ReactiveFormsModule} from '@angular/forms';
+import { GramsPipe } from './pipes/grams.pipe';
+import { MillimeterPipe } from './pipes/millimeter.pipe';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,6 +49,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     Matstems2Component,
     GramsPipe,
     DollarsPerGramPipe,
+    Matstems3Component,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -55,8 +64,22 @@ import {ReactiveFormsModule} from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+
   ],
-  providers: [],
+  providers: [    {
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://48p1r2roz4.sse.codesandbox.io'
+        })
+      }
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
