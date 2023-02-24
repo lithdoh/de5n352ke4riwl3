a10b970a6a1e58c60e720bd5ceccdd5cce919a3a
Mitch K <lithifuse@gmail.com>
2023-02-24 11:53:00 -0800

    initial commit

---
+++ src/app/getStemImages.sh
@@ -0,0 +1,150 @@
+wget content.competitivecyclist.com/images/items/large/FSA/FSAD12K/BLA.jpg -O fsa-gradient-35mm-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/REN/REN0025/BLA.jpg -O renthal-apex-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/SYN/SYN0005/BK.jpg -O syntace-force-109_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/SYN/SYN000B/BK.jpg -O syntace-megaforce2-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/EAS/EAS002P/BK.jpg -O easton-ea70-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/EAS/EAS002Q/BK.jpg -O easton-ea90-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/THP/THP000A/BK.jpg -O thomson-x4-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/REN/REN0029/BLA.jpg -O renthal-apex-35-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/TRU/TRUB10J/BLA.jpg -O truvativ-atmos-7k-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/ENV/ENV0060/BK.jpg -O enve-m7-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01P/BLA.jpg -O chromag-bza-35-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01P/BLU.jpg -O chromag-bza-35-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01P/RED.jpg -O chromag-bza-35-stem_RED.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01F/BLA.jpg -O chromag-hifi-35-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01F/BLU.jpg -O chromag-hifi-35-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01F/RED.jpg -O chromag-hifi-35-stem_RED.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/TRU/TRU000Y/BK.jpg -O truvativ-descendant-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001F/BLA.jpg -O pnw-components-range-v3-35-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000I/BLU500DE.jpg -O race-face-atlas-stem_0DE.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/ENV/ENV2SPZ/BLA.jpg -O enve-d35-alloy-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005D/BK.jpg -O race-face-aeffect-r-35-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF004R/BK.jpg -O race-face-ride-xc-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF001V/BK.jpg -O race-face-turbine-35-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000U/S65MM.jpg -O race-face-atlas-35-stem_5MM.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01J/BLU.jpg -O chromag-ranger-v2-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01J/PUR.jpg -O chromag-ranger-v2-stem_PUR.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01J/RED.jpg -O chromag-ranger-v2-stem_RED.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01E/ANOORA.jpg -O chromag-hifi-v2-stem_ORA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01E/BLA.jpg -O chromag-hifi-v2-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01E/BLU.jpg -O chromag-hifi-v2-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01E/RED.jpg -O chromag-hifi-v2-stem_RED.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA01E/SIL.jpg -O chromag-hifi-v2-stem_SIL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001E/BLA.jpg -O pnw-components-range-v3-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000J/BK.jpg -O race-face-turbine-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/PNW/PNW0011/BLA.jpg -O pnw-components-the-coast-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/TRU/TRU001W/BLA.jpg -O truvativ-descendant-35-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0009/BK.jpg -O deity-components-cavity-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0009/PLASI.jpg -O deity-components-cavity-stem_ASI.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0009/RD.jpg -O deity-components-cavity-stem_RD.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0009/PU.jpg -O deity-components-cavity-stem_PU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0009/BL.jpg -O deity-components-cavity-stem_BL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/ENV/ENV2SPY/BLA.jpg -O enve-d31-alloy-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/OUC/OUC0012/BLA.jpg -O oneup-components-edc-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/ENV/ENV2SQ3/BLA.jpg -O enve-m6-v2-stem_BLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/PLA.jpg -O deity-components-copperhead-stem_PLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/BK.jpg -O deity-components-copperhead-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/PU.jpg -O deity-components-copperhead-stem_PU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/RD.jpg -O deity-components-copperhead-stem_RD.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/BL.jpg -O deity-components-copperhead-stem_BL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/OR.jpg -O deity-components-copperhead-stem_OR.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000J/BZ.jpg -O deity-components-copperhead-stem_BZ.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/RCF/RCF004Q/BLU.jpg -O race-face-turbine-r-35-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/BK.jpg -O deity-components-copperhead-35mm-stem_BK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/RD.jpg -O deity-components-copperhead-35mm-stem_RD.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/PU.jpg -O deity-components-copperhead-35mm-stem_PU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/BL.jpg -O deity-components-copperhead-35mm-stem_BL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/OR.jpg -O deity-components-copperhead-35mm-stem_OR.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/PLA.jpg -O deity-components-copperhead-35mm-stem_PLA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Y/BZ.jpg -O deity-components-copperhead-35mm-stem_BZ.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/BLU.jpg -O industry-nine-a318-stem_BLU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/GOL.jpg -O industry-nine-a318-stem_GOL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/GRE.jpg -O industry-nine-a318-stem_GRE.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/LIM.jpg -O industry-nine-a318-stem_LIM.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/ORA.jpg -O industry-nine-a318-stem_ORA.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/PIN.jpg -O industry-nine-a318-stem_PIN.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/PUR.jpg -O industry-nine-a318-stem_PUR.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/RED.jpg -O industry-nine-a318-stem_RED.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/SIL.jpg -O industry-nine-a318-stem_SIL.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN1WU7/TUR.jpg -O industry-nine-a318-stem_TUR.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/TU.jpg -O industry-nine-a35-stem_TU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/LM.jpg -O industry-nine-a35-stem_LM.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/RD.jpg -O industry-nine-a35-stem_RD.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/PU.jpg -O industry-nine-a35-stem_PU.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/PK.jpg -O industry-nine-a35-stem_PK.jpg
+sleep 2
+wget content.competitivecyclist.com/images/items/large/NIN/NIN003G/GR.jpg -O industry-nine-a35-stem_GR.jpg
+sleep 2
