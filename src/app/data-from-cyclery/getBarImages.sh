097b744774391fc1cf7a009e992e172057480665
lithdoh <lithifuse@gmail.com>
2023-02-28 20:10:51 -0800

    Create script to get bars images and rename them

---
+++ src/app/data-from-cyclery/getBarImages.sh
@@ -0,0 +1,135 @@
+# The current images are all size medium, I've updated the links to large now
+wget content.competitivecyclist.com/images/items/large/TRU/TRU001J/BK.jpg -O truvativ-descendant-35mm-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000Y/TU.jpg -O race-face-sixc-dh-handlebar_TU.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000Y/RD.jpg -O race-face-sixc-dh-handlebar_RD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005N/STE.jpg -O race-face-turbine-r-35-35mm-rise-handlebar_STE.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005K/CAR.jpg -O race-face-sixc-35-35mm-rise-handlebar_CAR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/BLU.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_BLU.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/BRO.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_BRO.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/ORA.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_ORA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/RED.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_RED.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/TUR.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_TUR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0017/WHI.jpg -O deity-components-os-35-highside-50mm-rise-handlebar_WHI.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/WPC/WPC000T/MATBK.jpg -O whisky-parts-co.-no.9-carbon-flat-handlebar_MATBK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/ENV/ENV005Y/S318MMCLA.jpg -O enve-m9-handlebar_S318MMCLA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005J/BL.jpg -O race-face-sixc-35-20mm-rise-handlebar_BL.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005J/CAR.jpg -O race-face-sixc-35-20mm-rise-handlebar_CAR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005J/GN.jpg -O race-face-sixc-35-20mm-rise-handlebar_GN.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RIT/RITA0AC/BLA.jpg -O ritchey-comp-flat-handlebar_BLA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002C/GD.jpg -O renthal-fatbar-35-handlebar_GD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002C/BK.jpg -O renthal-fatbar-35-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/BLU.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_BLU.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/BRO.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_BRO.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/ORA.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_ORA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/RED.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_RED.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/TUR.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_TUR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC0018/WHI.jpg -O deity-components-os-35-highside-80mm-rise-handlebar_WHI.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Q/STE.jpg -O deity-components-bf800-brendog-handlebar_STE.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Q/WH.jpg -O deity-components-bf800-brendog-handlebar_WH.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Q/RD.jpg -O deity-components-bf800-brendog-handlebar_RD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/DTC/DTC000Q/OR.jpg -O deity-components-bf800-brendog-handlebar_OR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002G/GD.jpg -O renthal-fatbar-lite_GD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002G/BK.jpg -O renthal-fatbar-lite_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005G/CAR.jpg -O race-face-next-r-35-10mm-rise-handlebar_CAR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF005F/BK.jpg -O race-face-aeffect-r-35-20mm-rise-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PRS/PRSJ00Y/S35MCLA.jpg -O pro-tharsis-3five-carbon-handlebar_S35MCLA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/BK.jpg -O race-face-atlas-35-35mm-rise-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/BL.jpg -O race-face-atlas-35-35mm-rise-handlebar_BL.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/RD.jpg -O race-face-atlas-35-35mm-rise-handlebar_RD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/STE.jpg -O race-face-atlas-35-35mm-rise-handlebar_STE.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/PU.jpg -O race-face-atlas-35-35mm-rise-handlebar_PU.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF008H/GN.jpg -O race-face-atlas-35-35mm-rise-handlebar_GN.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002D/GD.jpg -O renthal-fatbar-lite-35-handlebar_GD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002D/BK.jpg -O renthal-fatbar-lite-35-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/ENV/ENV005X/S35MMCLA.jpg -O enve-m7-handlebar_S35MMCLA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000F/BK.jpg -O race-face-next-riser-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000F/GN.jpg -O race-face-next-riser-handlebar_GN.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000F/STE.jpg -O race-face-next-riser-handlebar_STE.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000F/TUR.jpg -O race-face-next-riser-handlebar_TUR.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002B/UNICB.jpg -O renthal-fatbar-lite-carbon-35-handlebar_UNICB.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002F/GD.jpg -O renthal-fatbar_GD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/TRU/TRU001C/BK.jpg -O truvativ-descendant-riser-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002A/UNICB.jpg -O renthal-fatbar-carbon-35-handlebar_UNICB.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN0022/S10MMRIS.jpg -O renthal-fatbar-lite-carbon_S10MMRIS.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN0022/S30MMRIS.jpg -O renthal-fatbar-lite-carbon_S30MMRIS.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001H/CEMGRE.jpg -O pnw-components-range-v3-35-handlebar_CEMGRE.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001H/REARED.jpg -O pnw-components-range-v3-35-handlebar_REARED.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001H/SAFORA.jpg -O pnw-components-range-v3-35-handlebar_SAFORA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001H/SEATEA.jpg -O pnw-components-range-v3-35-handlebar_SEATEA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000E/BK.jpg -O race-face-next-3-4in-riser-handlebar_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/RCF/RCF000E/RD.jpg -O race-face-next-3-4in-riser-handlebar_RD.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/REN/REN002K/BK.jpg -O renthal-fatbar-carbon_BK.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/ENV/ENV005W/S318MMCLA.jpg -O enve-m5-handlebar_S318MMCLA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/ENV/ENV005V/S75MMRIS.jpg -O enve-m6-handlebar_S75MMRIS.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/ENV/ENV005V/S25MMRIS.jpg -O enve-m6-handlebar_S25MMRIS.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001G/REARED.jpg -O pnw-components-range-v3-handlebar_REARED.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001G/SAFORA.jpg -O pnw-components-range-v3-handlebar_SAFORA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/PNW/PNW001G/SEATEA.jpg -O pnw-components-range-v3-handlebar_SEATEA.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/OUC/OUC0011/BLA.jpg -O oneup-components-carbon-handlebar_BLA.jpg
+sleep 3
