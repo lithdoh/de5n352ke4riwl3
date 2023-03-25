7092e3b7436375fc83781f9a8a8eb3219439ce0a
lithdoh <lithifuse@gmail.com>
2023-03-25 09:20:43 -0700

    initial commit

---
+++ src/app/data-from-cyclery/getFrameImages.sh
@@ -0,0 +1,82 @@
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1SPW/CORREE.jpg -O evil-bikes-the-wreckoning-mountain-bike-frame-evb1spw.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/JLI/JLIC06T/DUSMAU.jpg -O juliana-joplin-mountain-bike-frame-2019.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/JLI/JLIC065/BLUSTE.jpg -O juliana-roubion-carbon-cc-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA02J/BLAGOL.jpg -O chromag-monk-dj-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA02T/RAW.jpg -O chromag-surface-voyager-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/NNR/NNR99BW/FADTOBLA.jpg -O niner-wfo-rdo-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/CHR/CHRA02R/CUS.jpg -O chromag-primer-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1WU5/PROPOW.jpg -O evil-bikes-the-faction-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB09Z/BLU.jpg -O ibis-exie-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1WUP/REIBLO.jpg -O evil-bikes-offering-ls-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1WUM/SPABOT.jpg -O evil-bikes-the-insurgent-ls-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1WU7/BLADRU.jpg -O evil-bikes-the-following-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB0AR/CHAGRI.jpg -O ibis-ripmo-af-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00QT/SACACORUBLMUPEGLCHFOLO.jpg -O specialized-s-works-epic-frameset.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00R2/SATCOPBLA.jpg -O specialized-stumpjumper-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB06D/STDEGRJAXCO.jpg -O ibis-ripmo-mountain-bike-frame-ibsb06d.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00R1/SADUBLPEBLCA.jpg -O specialized-s-works-stumpjumper-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB0AE/BADAPP.jpg -O ibis-ripley-carbon-4.0-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB0AW/END.jpg -O ibis-ripmo-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1BC/TUR.jpg -O yeti-cycles-arc-turq-mountain-bike-frame-ytir1bc.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/NRO/NRO000A/GREGOL.jpg -O norco-optic-carbon-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1SPU/PROPOW.jpg -O evil-bikes-the-following-mountain-bike-frame-evb1spu.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB0AB/MUSSTA.jpg -O ibis-ripley-af-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/EVB/EVB1WUD/BAJFOG.jpg -O evil-bikes-the-wreckoning-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00RD/SAREONBL.jpg -O specialized-stumpjumper-evo-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00R4/SABLONBL.jpg -O specialized-enduro-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB05G/REDSAV.jpg -O ibis-ripmo-af-coil-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1HE/RAW.jpg -O yeti-cycles-sb140-turq-29in-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00RC/SATALUGUN.jpg -O specialized-stumpjumper-evo-alloy-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00RA/GLLISIBRDRSIGLBL.jpg -O specialized-fuse-m4-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SNZ/SNZK1EQ/GLOBAC.jpg -O santa-cruz-bicycles-jackal-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB0BM/PURCRU.jpg -O ibis-dv9-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SNZ/SNZK1JT/GLORED.jpg -O santa-cruz-bicycles-5010-carbon-cc-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/IBS/IBSB05F/MET.jpg -O ibis-ripmo-af-mountain-bike-frame-ibsb05f.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/NRO/NRO000U/GREGRE.jpg -O norco-sight-carbon-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1DE/MOS.jpg -O yeti-cycles-sb150-turq-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1G8/DUS.jpg -O yeti-cycles-sb120-turq-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1BP/ANT.jpg -O yeti-cycles-sb115-turq-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SCQ/SCQ00RB/GLSMCABL.jpg -O specialized-s-works-stumpjumper-evo-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/YTI/YTIR1C6/RAWCAR.jpg -O yeti-cycles-sb130-turq-mountain-bike-frame.jpg
+sleep 3
+wget content.competitivecyclist.com/images/items/large/SNZ/SNZK1FJ/GLOAQU.jpg -O santa-cruz-bicycles-tallboy-mountain-bike-frame.jpg
+sleep 3
