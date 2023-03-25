23ba68050ec1064440acbb8567a864fb4c8475af
lithdoh <lithifuse@gmail.com>
2023-03-25 09:21:04 -0700

    Add status component

---
+++ src/app/status/status.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  template: `
+    <h4>
+      404 Error: Requested page not found.
+    </h4>
+  `,
+  styles: [
+    `h4 {
+      padding-top: 150px;
+      padding-left: 25%;
+      font-size: 40px;
+      }`
+  ]
+})
+export class StatusComponent {
+
+}
