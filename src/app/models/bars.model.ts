7e3ecb447466f4213a418a6a1e7dcbc38b08c901
lithdoh <lithifuse@gmail.com>
2023-04-05 10:28:04 -0700

    initial commit

---
+++ src/app/models/bars.model.ts
@@ -0,0 +1,17 @@
+export interface Bars {
+    link: string;
+    image: string;
+    name: string;
+    brand: string;
+    diameter: number;
+    rise: number;
+    upsweep: number;
+    backsweep: number;
+    width: number;
+    color: string;
+    material: string;
+    weight: number;
+    price: number;
+  }
+  
+  
